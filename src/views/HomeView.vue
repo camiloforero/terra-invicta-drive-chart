5b96ff0058a233d875287e212f1168b710027f55
Camilo <camilo.forero@hey.com>
2023-11-19 07:50:11 -0500

    basic interactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import * as d3 from "d3";
-import { reactive, onMounted } from "vue"
+import { ref, reactive, watch, onMounted } from "vue"
 
 import { loadDataFromVersion, getDataForOptions } from '@/core/main'
+
+await loadDataFromVersion('0.3.107')
 
 // Options that affect the final calculation
 const options = reactive({
@@ -10,20 +12,41 @@ const options = reactive({
   payload: 2000
 })
 
-await loadDataFromVersion('0.3.107')
-const driveData = getDataForOptions(options)
+let driveData = getDataForOptions(options)
+
+// Interactivity
+watch(options, (newOptions) => {
+  console.log(newOptions)
+  update(getDataForOptions(options))
+})
 
 
 
-
-
-// D3 constants
+// D3 stuff
 const width = 800
 const height = 600
 const marginTop = 25;
 const marginRight = 20;
 const marginBottom = 35;
 const marginLeft = 40;
+
+// Reference to the G HTML element to be manipulated
+const g = ref(null)
+
+
+// Create the scales
+const x = d3.scaleLog()
+  .domain([1, 1e4])
+  .range([marginLeft, width - marginRight])
+
+const y = d3.scaleLog()
+  .domain([0.5, 1e5])
+  .range([height - marginBottom, marginTop])
+
+//scale for colors per drive type
+const driveTypeColor = d3.scaleOrdinal()
+  .domain([])
+  .range(d3.schemeSet3)
 
 onMounted(() => {
   // Create the SVG container
@@ -32,27 +55,8 @@ onMounted(() => {
     .attr("height", height)
     .attr("viewBox", [0, 0, width, height])
     .attr("style", "max-width: 100%; height: auto;");
-
-  const g = svg.append("g")
-
-  // Create the scales
-  const x = d3.scaleLog()
-    .domain([2, 1e4])
-    .range([marginLeft, width - marginRight])
-
-  const y = d3.scaleLog()
-    .domain([0.5, 1e5])
-    .range([height - marginBottom, marginTop])
-  
-  //scale for colors per drive type
-  const driveTypeColor = d3.scaleOrdinal()
-    .domain([])
-    .range(d3.schemeSet3)
   
   // Create and append the axes
-  const xAxis = d3.axisBottom(x)
-  const yAxis = d3.axisLeft(y)
-
   svg.append('g')
     .attr('transform', `translate(0,${height - marginBottom})`)
     .call(d3.axisBottom(x));
@@ -60,17 +64,20 @@ onMounted(() => {
   svg.append('g')
     .attr('transform', `translate(${marginLeft},0)`)
     .call(d3.axisLeft(y));
+  update(driveData)
+})
 
-  g
+function update(data) {
+  d3.select(g.value)
     .selectAll('circle')
-    .data(driveData)
+    .data(data)
     .join('circle')
     .attr('cx', (d) => x(d.drives[0].selectedOptionValues?.deltaV))
     .attr('cy', (d) => y(d.drives[0].selectedOptionValues?.accel))
     .attr('r', 5)
     .attr('title', (d) => d.drives[0].dataName)
     .style('fill', (d) => driveTypeColor(d.powerPlant?.powerPlantClass))
-})
+}
 
 
 </script>
@@ -78,8 +85,13 @@ onMounted(() => {
 <template>
   <main>
     <label>Fuel tanks
-    <input type="number" v-model="options.numFuelTanks"/>
+      <input type="number" v-model="options.numFuelTanks"/>
     </label>
-    <svg></svg>
+    <label>Payload
+      <input type="number" v-model="options.payload"/>
+    </label>
+    <svg>
+      <g ref="g"></g>
+    </svg>
   </main>
 </template>
